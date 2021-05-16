835cebe7079fb085cba49993c94520be6d7c5900
Victoriia Murakhovska <inbox@victoriia.online>
2021-05-17 00:16:26 +0300

    Version 21.02 (Test module)

--- lcd.py
+++ lcd.py
@@ -44,8 +44,7 @@ class Display():
         self.lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
                       cols=20, rows=4, dotsize=8,
                       charmap='A00',
-                      auto_linebreaks=True,
-                      backlight_enabled=True)
+                      auto_linebreaks=True)
 
     def rus_test(self):
         line = 'БГДЖЗИЙЛПУФЦЧШЩЬЫЮЯ     '
